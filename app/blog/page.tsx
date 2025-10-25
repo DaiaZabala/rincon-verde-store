c981287d05ec631704f3442245a4b7c4f3a264ec
DaiaZabala <daiana.azabala@gmail.com>
2025-10-25 20:05:31 -0300

    cambios en blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/header"
+import Header from "../../components/header"
 import { Footer } from "@/components/footer"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
